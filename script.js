8a5587e6fa8200ce0cf72479916096874fa0bcd3
Talupula Naveen <stylistar05@gmail.com>
2025-07-18 23:03:13 +0530

    Implement feedback functionality

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const cartContainer = document.getElementById("cart-container");
 const countProducts = document.getElementById("countProducts");
 const sortProducts = document.getElementById("sort");
 const clearCart = document.getElementById("clear");
+const feedBack = document.getElementById("feedback");
 
 const products = [
   {
@@ -45,7 +46,7 @@ function addToCart(id) {
     return product.id === id;
   });
   if (isAvailable) {
-    alert(`Item already added`);
+    userFeedback("Item already added", "error");
     return;
   }
 
@@ -54,6 +55,7 @@ function addToCart(id) {
   });
   myCart.push(productToAdd);
   renderCartDetails();
+  userFeedback(`${productToAdd.name} is added to the cart`, "success");
 }
 function renderCartDetails() {
   cartContainer.innerHTML = "";
@@ -71,19 +73,19 @@ function renderCartDetails() {
 }
 
 function removeFromCart(id) {
+  const product = myCart.find((product) => product.id === id);
   const productIndex = myCart.findIndex((item) => {
     return item.id === id;
   });
   myCart.splice(productIndex, 1);
+  userFeedback(`${product.name} is removed from the cart`, "error");
   renderCartDetails();
 }
 
 clearCart.addEventListener("click", () => {
-  renderCartDetails();
-});
-clearCart.addEventListener("click", () => {
   myCart.length = 0;
   renderCartDetails();
+  userFeedback("Cart is cleared", "success");
 });
 
 sortProducts.addEventListener("click", () => {
@@ -92,3 +94,21 @@ sortProducts.addEventListener("click", () => {
   });
   renderCartDetails();
 });
+
+let timerId;
+function userFeedback(msg, type) {
+  clearTimeout(timerId);
+  feedBack.style.display = "block";
+  if (type === "success") {
+    feedBack.style.backgroundColor = "green";
+  }
+  if (type === "error") {
+    feedBack.style.backgroundColor = "red";
+  }
+  feedBack.textContent = msg;
+
+  timerId = setTimeout(function () {
+    feedBack.style.display = "none";
+  }, 3000);
+}
+renderCartDetails();
